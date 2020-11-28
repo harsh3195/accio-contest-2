8e783b30b8c3f50db5f47db4847848edecf45393
harsh <harsharma.3195@gmail.com>
2020-11-29 02:30:45 +0530

    changes

--- MultipleX.java
+++ MultipleX.java
@@ -5,7 +5,7 @@ import java.util.*;
 import java.lang.*;
 import java.io.*;
 
-class GFG {
+class MultipleX {
 	public static void main (String[] args) {
 	            
 	            Scanner sc=new Scanner(System.in);
--- SmallMiss.java
+++ SmallMiss.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.lang.*;
 import java.io.*;
-class GFG
+class SmallMiss
  {
 	public static void main (String[] args)
 	 {
