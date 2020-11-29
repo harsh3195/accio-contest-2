2a699309387f439c2a49527febb1d2582cdba1a2
harsh <harsharma.3195@gmail.com>
2020-11-29 12:28:34 +0530

    changes

--- NMeetings.java
+++ NMeetings.java
@@ -28,35 +28,8 @@ class Meeting
 {
     static void maxMeetings(int start[], int end[], int n)
     {
-        int c=-1;
-        Pair[] act = new Pair[n];
-        for(int i=0;i<n;i++)
-            act[i] = new Pair(start[i],end[i],i+1);
-        Arrays.sort(act,new com());
-        for(int i=0;i<n;i++)
-            if(act[i].x>c)
-            {
-                c=act[i].y;
-                System.out.print(act[i].z+" ");
-            }
+        //your code here
     }   
-}
-class Pair
-{
-    int x,y,z;
-    Pair(int a,int b,int c)
-    {
-        x=a;
-        y=b;
-        z=c;
-    }
-}
-class com implements Comparator<Pair>
-{
-    public int compare(Pair a,Pair b)
-    {
-        return a.y-b.y;
-    }
 }
 
 
--- SmallMiss.java
+++ SmallMiss.java
@@ -6,26 +6,5 @@ class SmallMiss
 	public static void main (String[] args)
 	 {
 	 //code
-	    Scanner sc = new Scanner(System.in);
-	    int t = sc.nextInt();
-	    while(t-- > 0) {
-	        int n = sc.nextInt();
-	        n++;
-	        int array[] = new int[n];
-	        
-	        for (int i = 0; i < n-1; i++) {
-	            int d = sc.nextInt();
-	            if(d > 0 && d <= n) {
-	                 array[d - 1] = -1;
-	            }
-	        }
-	        
-	        for (int i =0; i < n;i++) {
-	            if (array[i] == 0) {
-	                System.out.println(i+1);
-	                break;
-	            }
-	        }
-	    }
 	 }
 }
